1dc66c2cdbb789d5c03cfdcf934dc4f18d6619c9
Mr.py09v <yapranav3@gmail.com>
2022-11-02 19:22:16 +0530

    adding python gui tutorial folder

---
+++ #7python GUI/#1firstGUI.py
@@ -0,0 +1,11 @@
+#importing tkinter for python gui
+from tkinter import * 
+
+#taking a instance from a gui class
+gui_root = Tk()
+
+#we perform gui logic here
+#it will create a small window and store logic of gui
+gui_root.mainloop()
+
+
---
+++ #7python GUI/#2labelGeomentry,max,min.py
@@ -0,0 +1,14 @@
+#importing tkinter for python gui
+from tkinter import * 
+
+#taking a instance from a gui class
+gui_root = Tk()
+
+
+#windows api will handle tkinter gui also with cross platform
+#this used to set size of a window
+#we will write in widthxheight
+gui_root.geometry("644x344")
+
+
+gui_root.mainloop()
