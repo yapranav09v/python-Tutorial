8d8083191cf72161c066b1b2a28ebbee67ca5b55
Mr.py09v <yapranav3@gmail.com>
2022-11-03 17:43:40 +0530

    added some lines in attribut file

--- #7python GUI/#4Attributsof_label_pack.py
+++ #7python GUI/#4Attributsof_label_pack.py
@@ -30,6 +30,14 @@ bg="yellow",fg="red",padx=44,pady=44,font=("comicsansms", 11, "bold"),borderwidt
 next_label = Label(text="the full form of gui is graphical user interface\nit is used to make attractice programs",
 bg="red",fg="yellow",padx=88,pady=88,font="comicsansms 30 italic",borderwidth=3,relief=GROOVE)
 
-text_label.pack()
-next_label.pack()
+#important pack options
+#anchor = we can change it by sounth, north in top
+#anchor="ne"
+#side = top, bottom, left, right
+
+#moving text_label to the North-East at top it will fill at y
+text_label.pack(anchor="ne",fill=Y, padx=85, pady=59)
+#moving next_label to the South-East but it will not go at bottom(because its always at top/midle by default) without specifing side's
+#using fill it will fill at x when we pull a window at x
+next_label.pack(side=BOTTOM, anchor="se", fill=X)
 user_root.mainloop()
